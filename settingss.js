9af9baadf6d7f74042503423f733da99a81eb894
rayzerrazack-sudo <rayzerrazack@gmail.com>
2025-09-20 11:55:53 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
+  SESSION_ID: "malvin~FidWlADK#ufzvuOFmzE8QfUqk8eKQ1ROK2bzicOW3sqvmD39X9qM",  // add your Session Id here
+  OWNER_NUMBER: "255793558218", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
-  TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Tanzania" //put your country timeZone....leave blank if u don't know.
 };
